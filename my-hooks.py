8d3eb1cc446beeb25bbc237fcd9ea4c3180801cf
dilipc1990 <dilipc1990@gmail.com>
2022-10-24 11:29:06 -0400

    Update my-hooks.py

--- my-hooks.py
+++ my-hooks.py
@@ -1,4 +1,4 @@
 def zap_pre_shutdown(zap):
   print(zap.spider.all_urls)
-  print(zap.stats.site_stats("https://demo.testfire.net/", "stats.auth"))
+  print(zap.stats.site_stats("https://demo.testfire.net/"))
   print(zap.stats.stats)
